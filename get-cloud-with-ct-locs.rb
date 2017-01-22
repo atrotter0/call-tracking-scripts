d26c929eb6b0d265e5a80bc4038e6f5fb305619a
Abel Trotter <abel.trotter@getg5.com>
2017-01-21 20:27:00 -0800

    Add more filters

--- get-cloud-with-ct-locs.rb
+++ get-cloud-with-ct-locs.rb
@@ -3,7 +3,7 @@ locations = Location.where(no_call_tracking: true)
 
 locations.each do |loc|
   client = Client.where(id: loc.client_id).first
-  if client.present? && client.status != "Core-Active"
+  if client.present? && client.status != "Core-Active" && client.status != "Deleted" && client.g5_internal != true && loc.status != "Deleted"
     filtered_list << loc.urn
   end
 end
